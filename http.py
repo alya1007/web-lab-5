7af3af15d2c43307e45d9e036342ea54556dcecf
alya1007 <alexandra.konjevic@isa.utm.md>
2024-03-27 22:48:12 +0200

    feat: add support for redirects

--- http.py
+++ http.py
@@ -3,6 +3,7 @@ from typing import Dict
 from urllib.parse import urlparse
 import socket
 import ssl
+from bs4 import BeautifulSoup as bs
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 
